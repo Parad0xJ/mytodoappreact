5e499e451bca39f12327f64dbcd4d8f6f564abfc
Parad0xJ <paradoxlyon@gmail.com>
2019-07-21 00:50:30 +0200

    Updates

---
+++ precache-manifest.c68a799577df759ec5eba0255f98b848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a961a06237128ea64b42eaff8fc73975",
+    "url": "/mytodoappreact/index.html"
+  },
+  {
+    "revision": "0f8178c8dffeb688a534",
+    "url": "/mytodoappreact/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d0f26f356373b0e04fb3",
+    "url": "/mytodoappreact/static/css/main.c3db600d.chunk.css"
+  },
+  {
+    "revision": "0f8178c8dffeb688a534",
+    "url": "/mytodoappreact/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "d0f26f356373b0e04fb3",
+    "url": "/mytodoappreact/static/js/main.b65ee64a.chunk.js"
+  },
+  {
+    "revision": "1e0c033e371446f43848",
+    "url": "/mytodoappreact/static/js/runtime~main.29ce7607.js"
+  }
+]);
